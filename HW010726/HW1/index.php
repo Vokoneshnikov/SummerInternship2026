0564916cda6274ae3650058d8e327474b3ec9646
Vladimir Okoneshniko <Vladimir.Okoneshniko@shire.local>
2026-07-02 10:44:38 +0300

    hw1

--- HW010726/HW1/index.php
+++ HW010726/HW1/index.php
@@ -1,1 +1,36 @@
 <?php
+function isNameOk(string $name) : bool{
+    return !empty($name);
+};
+
+function isAgeOk(string $age) : bool {
+    return (!empty($age) && is_numeric($age) && $age >= 0 && $age <= 130);
+};
+
+echo "Введите имя: \n";
+$name = trim(fgets(STDIN));
+
+while (!isNameOk($name)) {
+    echo "Некорректный ввод имени \n";
+
+    echo "Введите имя: \n";
+
+    $name = trim(fgets(STDIN));
+}
+
+echo "Введите возраст: \n";
+
+$age = trim(fgets(STDIN));
+
+while (!isAgeOk($age)) {
+    echo "Некорректный ввод возраста \n";
+
+    echo "Введите возраст: \n";
+
+    $age = trim(fgets(STDIN));
+}
+
+$ageGroup = $age > 18 ? "adult" : "child";
+
+$result = "Привет, {$name}. Ты - {$ageGroup}";
+fwrite(STDOUT, $result);
