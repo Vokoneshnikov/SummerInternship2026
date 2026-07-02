bbd1ecc55d396afcd85e5b8f1a6ce0eb9b5917a1
Vladimir Okoneshniko <Vladimir.Okoneshniko@shire.local>
2026-07-02 10:57:44 +0300

    hw2

--- HW010726/HW2/index.php
+++ HW010726/HW2/index.php
@@ -1,1 +1,47 @@
 <?php
+function getSumOfArray(array $array) : float {
+    $result = 0;
+    foreach ($array as $value) {
+        $result += $value;
+    }
+    return $result;
+};
+function getMaxOfArray(array $array) : float {
+    $result = PHP_FLOAT_MIN;
+
+    foreach ($array as $value) {
+        $result = max($value, $result);
+    }
+
+    return $result;
+};
+function getMinOfArray(array $array) : float {
+
+    $result = PHP_FLOAT_MAX;
+
+    foreach ($array as $value) {
+        $result = min($value, $result);
+    }
+
+    return $result;
+};
+
+$file = fopen("file.txt", "r");
+
+$arr = [];
+
+$line = fgets($file);
+
+while ($line !== false) {
+    $arr[] = (float)$line;
+
+    $line = fgets($file);
+}
+fclose($file);
+
+
+echo "Сумма всех чисел: " . getSumOfArray($arr) . "\n";
+
+echo "Максимальное число: " . getMaxOfArray($arr) . "\n";
+
+echo "Минимальное числоа: " . getMinOfArray($arr) . "\n";
