a4aee123fffa1ec6de8bfe12c524b13a619dad81
Vokoneshnikov <okoneshnikov.vova@gmail.com>
2026-07-10 14:44:55 +0300

    locks and syncState checks

--- InternetShopAPI/src/Command/UpdatePricesCommand.php
+++ InternetShopAPI/src/Command/UpdatePricesCommand.php
@@ -2,24 +2,43 @@
 
 namespace App\Command;
 
+use App\Enums\SyncType;
 use App\Service\ProductService;
+use App\Service\SyncStateService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Lock\LockFactory;
 
 #[AsCommand("app:update-prices")]
 class UpdatePricesCommand extends Command {
-
-    public function __construct(private ProductService $productService) {
+    private const LOCK_TTL = 120;
+    public function __construct(
+        private ProductService $productService,
+        private SyncStateService $syncStateService,
+        private LockFactory $lockFactory
+    ) {
         parent::__construct();
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int {
+
+        $io = new SymfonyStyle($input, $output);
+
+        $lock = $this->lockFactory->createLock('update_prices', self::LOCK_TTL);
+
+        if (!$lock->acquire()) {
+            $io->warning('Поток занят');
+            return Command::SUCCESS;
+        }
+
         try {
-            $io = new SymfonyStyle($input, $output);
-            $this->productService->updateProducts();
+            if (!$this->syncStateService->isUpdated(SyncType::PRICES)) {
+                $this->productService->updateProducts();
+                $this->syncStateService->updateTime(SyncType::PRICES);
+            }
 
             return Command::SUCCESS;
         }
@@ -27,6 +46,8 @@ class UpdatePricesCommand extends Command {
             $io->error($e->getMessage());
 
             return Command::FAILURE;
+        } finally {
+            $lock->release();
         }
     }
 }
--- InternetShopAPI/src/Command/UpdateRatesCommand.php
+++ InternetShopAPI/src/Command/UpdateRatesCommand.php
@@ -2,31 +2,50 @@
 
 namespace App\Command;
 
+use App\Entity\SyncState;
+use App\Enums\SyncType;
 use App\Service\ExchangeRatesService;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\SyncStateService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Lock\LockFactory;
 
 #[AsCommand('app:update-rates')]
 class UpdateRatesCommand extends Command {
-    public function __construct(private ExchangeRatesService $exchangeRatesService) {
+    private const LOCK_TTL = 120;
+    public function __construct(
+        private ExchangeRatesService $exchangeRatesService,
+        private SyncStateService $syncStateService,
+        private LockFactory $lockFactory,
+    ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int {
         $io = new SymfonyStyle($input, $output);
 
-        try {
-            $this->exchangeRatesService->updateRates();
+        $lock = $this->lockFactory->createLock('update_rates', self::LOCK_TTL);
 
+        if (!$lock->acquire()) {
+            $io->warning('Поток занят');
+            return Command::SUCCESS;
+        }
+
+        try {
+            if (!$this->syncStateService->isUpdated(SyncType::RATES)) {
+                $this->exchangeRatesService->updateRates();
+                $this->syncStateService->updateTime(SyncType::RATES);
+            }
             return Command::SUCCESS;
         } catch (\Exception $e) {
             $io->error($e->getMessage());
 
             return Command::FAILURE;
+        } finally {
+            $lock->release();
         }
     }
 }
