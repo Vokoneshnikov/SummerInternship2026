517741012e4c6eb3f3716d268604a0f7531eabf6
Vokoneshnikov <okoneshnikov.vova@gmail.com>
2026-07-10 16:18:10 +0300

    first test

---
+++ InternetShopAPI/src/Tests/UnitTests/ProductServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\UnitTests;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductServiceTest extends TestCase {
+
+
+    public function testSmth() {
+       $this->assertTrue(true);
+    }
+}
